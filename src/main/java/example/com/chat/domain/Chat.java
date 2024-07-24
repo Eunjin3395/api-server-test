0e2c170c9b438624e00005aa02c975c6151724f1
Eunjin3395 <jennyeunjin@khu.ac.kr>
2024-07-25 00:32:11 +0900

    timestamp 속성 및 util 추가

--- src/main/java/example/com/chat/domain/Chat.java
+++ src/main/java/example/com/chat/domain/Chat.java
@@ -2,8 +2,21 @@ package example.com.chat.domain;
 
 import example.com.common.domain.BaseDateTimeEntity;
 import example.com.member.domain.Member;
-import jakarta.persistence.*;
-import lombok.*;
+import example.com.utils.TimestampUtil;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.PrePersist;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
@@ -11,6 +24,7 @@ import lombok.*;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 public class Chat extends BaseDateTimeEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "chat_id")
@@ -19,6 +33,9 @@ public class Chat extends BaseDateTimeEntity {
     @Column(nullable = false, length = 1000)
     private String contents;
 
+    @Column(nullable = false)
+    private Long timestamp;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "chatroom_id", nullable = false)
     private Chatroom chatroom;
@@ -26,4 +43,10 @@ public class Chat extends BaseDateTimeEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "from_member_id", nullable = false)
     private Member fromMember;
+
+    // repository에 저장되기 전에 해당 timestamp를 자동으로 설정
+    @PrePersist
+    public void prePersist() {
+        this.timestamp = TimestampUtil.getNowUtcTimeStamp();
+    }
 }
--- src/main/java/example/com/utils/TimestampUtil.java
+++ src/main/java/example/com/utils/TimestampUtil.java
@@ -2,7 +2,7 @@ package example.com.utils;
 
 import java.time.Instant;
 
-public class timestamp {
+public class TimestampUtil {
 
     public static long getNowUtcTimeStamp() {
         // 현재 UTC 시간 기준 타임스탬프 생성
